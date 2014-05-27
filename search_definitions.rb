880a086f03549d786beefddf9ea16080c9c71303
Mark Triggs <mark@dishevelled.net>
2014-05-27 15:08:21 +1000

    Add a search for authorizers too

--- search_definitions.rb
+++ search_definitions.rb
@@ -1,1 +1,2 @@
 AdvancedSearch.define_field(:name => 'payment_fund_code', :type => :enum, :visibility => [:staff], :solr_field => 'payment_fund_code_u_utext')
+AdvancedSearch.define_field(:name => 'payment_authorizers', :type => :text, :visibility => [:staff], :solr_field => 'payment_authorizers_u_utext')
