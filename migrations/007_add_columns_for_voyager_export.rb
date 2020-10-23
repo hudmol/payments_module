22433157b390016416cdafafec4596e686f4af08
Mark Triggs <mark@dishevelled.net>
2020-10-23 11:56:46 +1100

    Piv#175311347 Update migration to read cost centers and spend categories from a TSV
    Removes Yale-specific values from the enumerations.

--- migrations/007_add_columns_for_voyager_export.rb
+++ migrations/007_add_columns_for_voyager_export.rb
@@ -1,30 +1,51 @@
 require 'db/migrations/utils'
+require 'csv'
+
+def load_tsv_into_enumeration(db, enumeration_name, prompt)
+  loop do
+    begin
+      $stderr.puts ""
+      $stderr.puts prompt
+      $stderr.flush
+      file = $stdin.readline.to_s.strip
+
+      break if file.empty?
+
+      codes = []
+      CSV.foreach(file, :col_sep => "\t") do |row|
+        code = row.map {|s| s.to_s.strip}.first
+
+        # Empty line
+        next if code.nil?
+
+        codes << code
+      end
+
+      enum_id = self[:enumeration][:name => enumeration_name][:id]
+
+      raise "Couldn't find an enumeration with name: #{enumeration_name}" unless enum_id
+
+      codes.each_with_index do |code, i|
+        self[:enumeration_value].insert(:enumeration_id => enum_id,
+                                        :value => code,
+                                        :position => i,
+                                        :readonly => 0)
+      end
+
+      # Success!
+      return
+    rescue
+      $stderr.puts "File '#{file}' doesn't exist or couldn't be opened: #{$!}"
+      sleep 1
+    end
+  end
+end
 
 Sequel.migration do
 
   up do
-    create_editable_enum('payments_module_cost_center',
-                         [
-                          'CC0681',
-                          'CC0682',
-                          'CC0683',
-                          'CC0684',
-                          'CC0685',
-                          'CC0686',
-                          'CC0687',
-                          'CC0688',
-                         ])
-
-    create_editable_enum('payments_module_spend_category',
-                         [
-                          'SC121',
-                          'SC120',
-                          'SC122',
-                          'SC670',
-                          'SC118',
-                          'SC669',
-                          'SC119',
-                         ])
+    create_editable_enum('payments_module_cost_center', [])
+    create_editable_enum('payments_module_spend_category', [])
 
     alter_table(:payment) do
       add_column(:date_paid, Date, :null => true)
@@ -36,7 +57,28 @@ Sequel.migration do
       add_column(:spend_category_id, Integer, :null => true)
       add_foreign_key([:spend_category_id], :enumeration_value, :key => :id, :name => 'payments_module_spend_category_fk')
     end
-  end
 
+    if !defined?(ASpaceEnvironment) || ASpaceEnvironment.environment == :production
+      $stderr.puts <<EOF
+
+You seem to be setting up the ArchivesSpace payment module for the first time.
+
+At this point, you can pre-load the system with a set of cost centers and spend
+categories if you have them ready.  You can always add and edit more values
+later.
+
+EOF
+
+      self.transaction do
+        load_tsv_into_enumeration(self,
+                                  'payments_module_cost_center',
+                                  'Please provide a text file containing one cost center code per line (leave blank to skip): ')
+
+        load_tsv_into_enumeration(self,
+                                  'payments_module_spend_category',
+                                  'Please provide a text file containing one spend category code per line (leave blank to skip): ')
+      end
+    end
+  end
 end
 
