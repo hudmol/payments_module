84277e82c67f5b7fd5f8b935ce9d9ec3d94608a1
James Bullen <jambun@gmail.com>
2020-10-27 12:05:30 +1100

    Piv#175278442 only allow admins to edit vendor code

--- backend/model/mixins/vendor_code.rb
+++ backend/model/mixins/vendor_code.rb
@@ -5,6 +5,14 @@ module VendorCode
 
   def validate
     super
+
+    if self.vendor_code != self.class.filter(:id => self.id).get(:vendor_code)
+      current_user = User[:username => RequestContext.get(:current_username)]
+      unless current_user.can?(:administer_system)
+        errors.add(:vendor_code, "Vendor code can only be changed by Administrators")
+      end
+    end
+
     validates_unique(:vendor_code, :message => "Vendor Code already in use")
 
     # we need to enforce uniqueness across the different agent models
